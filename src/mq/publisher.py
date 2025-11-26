432603ed75d12cf7059f68311733d33800be8c40
Ruzek <reek867@gmail.com>
2025-11-26 08:56:19 +0300

    Fix rabbitmq

--- src/mq/publisher.py
+++ src/mq/publisher.py
@@ -1,10 +1,16 @@
-from faststream.rabbit import RabbitBroker
+from faststream.rabbit import RabbitBroker, RabbitQueue
 from mq.router import rabbit_router
 
 
 class RabbitPublisher:
     def __init__(self, broker: RabbitBroker):
         self.broker = broker
+        self._declared_queues: set[str] = set()
+
+    async def _ensure_queue(self, queue: str):
+        if queue not in self._declared_queues:
+            await self.broker.declare_queue(RabbitQueue(queue))
+            self._declared_queues.add(queue)
 
     async def publish(
         self,
@@ -13,6 +19,8 @@ class RabbitPublisher:
         *,
         correlation_id: str | None = None,
     ) -> None:
+        await self._ensure_queue(queue=queue)
+
         await self.broker.publish(
             message,
             queue=queue,
